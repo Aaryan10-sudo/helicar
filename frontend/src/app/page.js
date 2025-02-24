17de6723bd5cc895a40d32e3a9eee68cb76f1ddc
Mingtindu <mingmartu@gmail.com>
2025-02-24 16:23:05 +0545

    feat: home page

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,95 +1,67 @@
+"use client";
+import { assets } from "@/assets/assets";
+import Blog from "@/components/common/Blog";
+import Hero from "@/components/common/Hero";
+import OurMission from "@/components/common/OurMission";
+import PopularVehicles from "@/components/common/PopularVehicle";
+import Rating from "@/components/common/Rating";
+import WhyUs from "@/components/common/WhyUs";
+import useGetVehicle from "@/hooks/useVehicle";
 import Image from "next/image";
-import styles from "./page.module.css";
+
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const { loading, vehicles } = useGetVehicle();
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+  return (
+    <div>
+      <section>
+       <Hero/>
+       <div className="px-4 sm:px-6 lg:px-8 w-full max-w-7xl mx-auto py-12 lg:py-20">
+          <div className="mb-8 lg:mb-12 flex flex-col gap-2.5">
+            <h1 className="text-3xl sm:text-center font-Comfortaa sm:text-4xl md:text-5xl text-primary font-bold">
+              Popular Services:
+            </h1>
+            <p className="text-[14px] md:text-[16px] text-start font-light leading-[18px] md:leading-[22px] max-w-[90%] md:max-w-[785px] text-subheading mx-auto">
+            Explore Nepal like never before with our top-notch vehicle rental services. 
+            Travel with ease and comfort, discovering breathtaking landscapes, rich cultures, and unforgettable experiences. 
+            Your adventure starts here!
+          </p>
+          </div>
+          <PopularVehicles loading={loading} limit={3} vehicles={vehicles} />
         </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        <WhyUs/>
+          <section className="w-full flex flex-col items-center py-10 px-4 lg:px-16">
+              {/* Heading Section */}
+              <div className="text-center mb-6 lg:mb-10">
+                <h1 className="text-3xl sm:text-4xl md:text-5xl font-Comfortaa text-primary font-bold">
+                  Blogs
+                </h1>
+                <p className="text-sm md:text-base text-gray-600 max-w-[90%] md:max-w-[785px] mx-auto mt-2">
+                  Explore Nepal like never before with our top-notch vehicle rental services. 
+                  Travel with ease and comfort, discovering breathtaking landscapes, rich cultures, and unforgettable experiences. 
+                  Your adventure starts here!
+                </p>
+              </div>
+        
+              {/* Content Section */}
+              <Blog limit={3} loading={loading} vehicles={vehicles} />
+              <OurMission/>
+              <div className="text-center mb-6 flex flex-col gap-4 lg:mb-10">
+               <div>
+               <h1 className="text-3xl sm:text-4xl md:text-5xl font-Comfortaa text-primary font-bold">
+                  What Clients Say About Us
+                </h1>
+                <p className="text-sm md:text-base text-gray-600 max-w-[90%] md:max-w-[785px] mx-auto mt-2">
+                  Explore Nepal like never before with our top-notch vehicle rental services. 
+                  Travel with ease and comfort, discovering breathtaking landscapes, rich cultures, and unforgettable experiences. 
+                  Your adventure starts here!
+                </p>
+               </div>
+                <Rating/>
+              </div>
+            </section>
+      </section>
     </div>
   );
 }
---
+++ frontend/src/components/common/RatingCard.js
@@ -0,0 +1,46 @@
+import { assets } from "@/assets/assets";
+import Image from "next/image";
+import React from "react";
+
+const RatingCard = ({ rating }) => {
+  const ratingStars = Array.from({ length: 5 }, (_, index) => index < rating.ratingValue);
+
+  return (
+    <div className="relative bg-[#F5F5F5] shadow-lg rounded-3xl flex items-center p-6 w-full max-w-[400px] sm:max-w-[325px] h-[160px]">
+      <div className="absolute left-[-33px] z-10 top-[-10px] border-subheading border-[2px] rounded-full overflow-hidden h-[186px] w-[110px]"></div>
+      <div className="absolute z-0 left-[-40px] rounded-full overflow-hidden h-[186px] w-[110px]">
+        <Image
+          src={rating.image.src}
+          height={rating.image.height}
+          width={rating.image.width}
+          alt="user image"
+          className="w-full h-full object-cover"
+        />
+      </div>
+
+      <div className="flex flex-col w-full justify-center items-start pl-20 pr-4">
+        <h1 className="text-[#222121] font-Comfortaa font-bold text-lg leading-7">
+          {rating.name}
+        </h1>
+        <p className="text-subheading text-start w-full text-sm font-light font-LeagueSpartan">
+          {rating.review}
+        </p>
+
+        <div className="flex mt-2">
+          {ratingStars.map((isFilled, index) => (
+            <Image
+              key={index}
+              src={rating.ratingImage.src}
+              height={rating.ratingImage.height}
+              width={rating.ratingImage.width}
+              alt="rating star"
+              className={isFilled ? "opacity-100" : "opacity-40"} 
+            />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default RatingCard;
