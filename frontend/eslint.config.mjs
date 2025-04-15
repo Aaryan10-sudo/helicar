06f43b58df90bd1f7e2f56869040a3ed83c8eee4
ngimatendi <ngimatendi12@gmail.com>
2025-04-15 10:42:27 +0545

    feat: eslint configuration in frontend

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -9,6 +9,25 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [...compat.extends("next/core-web-vitals")];
+const eslintConfig = [
+  ...compat.config({
+    extends: ["next/core-web-vitals", "prettier"],
+    rules: {
+      semi: ["error"],
+      quotes: ["error", "double"],
+      "prefer-arrow-callback": ["error"],
+      "prefer-template": ["error"],
+      "no-var": ["error"],
+      "no-unused-vars": ["error"],
+      "no-console": ["warn"],
+      "no-debugger": ["warn"],
+      "no-duplicate-imports": ["error"],
+      "no-undef": ["error"],
+      "no-unused-expressions": ["error"],
+      "no-duplicate-case": ["error"],
+      "no-empty": ["error"],
+    },
+  }),
+];
 
 export default eslintConfig;
