7930911908f41b14b548affc128f35a4f55a4429
Aaryan10-sudo <lazyfox916@gmail.com>
2025-06-20 17:32:31 +0545

    removed http-error

--- client/src/components/common/WhyUs.jsx
+++ client/src/components/common/WhyUs.jsx
@@ -44,7 +44,7 @@ const WhyUs = () => {
           {formData.header || "Why Choose Us?"}
         </h1>
         <p className="text-sm md:text-base text-gray-600 max-w-[90%] md:max-w-[785px] mx-auto mt-2">
-          {formData.description }
+          {formData.description}
         </p>
       </div>
 
@@ -53,13 +53,9 @@ const WhyUs = () => {
         {/* Main Image */}
         <div className="w-full lg:w-[45%] flex justify-center">
           <Image
-            src={
-              formData.mainImage
-                ? formData.mainImage
-                : assets.whyUs1.src
-            }
-            width={assets.whyUs1.width}
-            height={assets.whyUs1.height}
+            src={formData.mainImage ? formData.mainImage : assets.whyUs1.src}
+            width={assets?.whyUs1.width}
+            height={assets?.whyUs1.height}
             alt="Luxury rental vehicle in Nepal"
             className="w-full max-w-[553px] h-auto rounded-lg shadow-lg"
           />
@@ -71,10 +67,7 @@ const WhyUs = () => {
             <p className="text-center col-span-2">Loading features...</p>
           ) : formData.features && formData.features.length > 0 ? (
             formData.features.map((feature, idx) => (
-              <div
-                key={idx}
-                className="flex flex-col items-center text-center"
-              >
+              <div key={idx} className="flex flex-col items-center text-center">
                 <Image
                   src={feature.image || assets.whyUs2.src}
                   width={assets.whyUs2.width}
@@ -86,9 +79,7 @@ const WhyUs = () => {
                   {feature.title}
                 </h2>
                 {feature.description && (
-                  <p className="text-gray-600 mt-1">
-                    {feature.description}
-                  </p>
+                  <p className="text-gray-600 mt-1">{feature.description}</p>
                 )}
               </div>
             ))
